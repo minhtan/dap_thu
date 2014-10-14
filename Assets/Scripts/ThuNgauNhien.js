2a7160512b657d784f8c8c096e946adf0c03064d
tuongpm <phamminhtuong0123@gmail.com>
2014-10-14 17:00:52 +0700

    tao moi script thu ngau nhien controlinput thay ten script trangthai

---
+++ Assets/Scripts/ThuNgauNhien.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+private var soDuocchon : int;
+
+
+function Start () {
+}
+
+function Update () {
+	
+}
+function NgauNhien(){
+	soDuocchon = Random.Range(1,12);
+	return soDuocchon;
+}
