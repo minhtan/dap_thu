306f1804d547e02272c3052563fcdebd690ecd16
Tan <m.tan2007@gmail.com>
2014-10-17 14:30:32 +0700

    add GUI skin

--- Assets/Scripts/HolePlacement.js
+++ Assets/Scripts/HolePlacement.js
@@ -1,15 +1,16 @@
 ﻿#pragma strict
 
+var spritePixelToUnit : int = 100;
+
 function Start () {
-//	var sHeight = Screen.height;
-//	var sWidth = Screen.width;
-//	var hHeight = gameObject.renderer.bounds.size.y;
-//	var hWidth = gameObject.renderer.bounds.size.x;
-//	
-//	Debug.Log("Screen height: "+sHeight);
-//	Debug.Log("Screen width: "+sWidth);
-//	Debug.Log("Hole height: "+hHeight);
-//	Debug.Log("Hole width: "+hWidth);
+	var sHeight = Screen.height;
+	var sWidth = Screen.width;
+	var hHeight = gameObject.renderer.bounds.size.y * spritePixelToUnit;
+	var hWidth = gameObject.renderer.bounds.size.x * spritePixelToUnit;
+	Debug.Log("Screen height: "+sHeight);
+	Debug.Log("Screen width: "+sWidth);
+	Debug.Log("Hole height: "+hHeight);
+	Debug.Log("Hole width: "+hWidth);
 }
 
 function Update () {
