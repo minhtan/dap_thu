c6278975e2cba3efcd2b299b338104f356c6079c
Tan <m.tan2007@gmail.com>
2014-10-17 08:24:30 +0700

    rearange script folder

--- Assets/Scripts/HolePlacement.js
+++ Assets/Scripts/HolePlacement.js
@@ -1,18 +1,15 @@
 ﻿#pragma strict
 
-var holeCount : int = 6;
-var holePrefab : GameObject;
-
 function Start () {
-	var sHeight = Screen.height;
-	var sWidth = Screen.width;
-	var hHeight = holePrefab.renderer.bounds.size.y;
-	var hWidth = holePrefab.renderer.bounds.size.x;
-	
-	Debug.Log("Screen height: "+sHeight);
-	Debug.Log("Screen width: "+sWidth);
-	Debug.Log("Hole height: "+hHeight);
-	Debug.Log("Hole width: "+hWidth);
+//	var sHeight = Screen.height;
+//	var sWidth = Screen.width;
+//	var hHeight = gameObject.renderer.bounds.size.y;
+//	var hWidth = gameObject.renderer.bounds.size.x;
+//	
+//	Debug.Log("Screen height: "+sHeight);
+//	Debug.Log("Screen width: "+sWidth);
+//	Debug.Log("Hole height: "+hHeight);
+//	Debug.Log("Hole width: "+hWidth);
 }
 
 function Update () {
