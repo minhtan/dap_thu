6d479a31a4b89fe495567d2fa26d1892b9eb879a
Tan <m.tan2007@gmail.com>
2014-11-01 12:44:01 +0700

    final version with mecanim, switch to new branch to test on legacy animation

--- Assets/Scripts/TrangThai.js
+++ Assets/Scripts/TrangThai.js
@@ -5,34 +5,17 @@ var showTime : float = 2.0f;
 //components reference
 private var anim : Animator;
 private var gameControl : GameControl;
+private var isHit : boolean;
 
 //points for this thu
 var thuPoint : int = 1;
-
-//click control
-private var clicked : boolean;
-var clickInterval : float;
-
-function isClicked(){
-	return clicked;
-}
-
-function click(){
-	clicked = true;
-	Invoke("unclick", clickInterval);
-}
-
-function unclick(){
-	clicked = false;
-}
-//end click control
 
 function getThuPoint(){
 	return thuPoint;
 }
 
 function isHitable(){
-	if(anim.GetCurrentAnimatorStateInfo(0).IsName("showed") || anim.GetCurrentAnimatorStateInfo(0).IsName("showing")){
+	if(!anim.GetCurrentAnimatorStateInfo(0).IsName("hidden") && !anim.GetCurrentAnimatorStateInfo(0).IsName("dying") && !anim.GetCurrentAnimatorStateInfo(0).IsName("hiding") && !isHit){
 		return true;
 	}else{
 		return false;
@@ -50,27 +33,26 @@ function isDead(){
 function Awake () {
 	anim = GetComponent.<Animator>();
 	gameControl = GameObject.Find("/HolesContainer").GetComponent.<GameControl>();
-	clicked = false;
 	anim.SetFloat("powerUp", 0.0);
+	isHit = false;
 }
 
 function die(){
 	anim.SetTrigger("die");
-}
-
-function show(){
-	anim.SetTrigger("show");
-	Invoke("hide", showTime);
+	CancelInvoke("hide");
+	isHit = true;
 }
 
 function show(powerUp : float){
 	anim.SetFloat("powerUp", powerUp);
 	anim.SetTrigger("show");
+	isHit = false;
 	Invoke("hide", showTime);
 }
 
 function hide(){
 	anim.SetTrigger("hide");
+	CancelInvoke("hide");
 	gameControl.miss();
 }
 
@@ -89,7 +71,5 @@ function checkPowerUp(){
 
 function getHit(){
 	checkPowerUp();
-	click();
-	CancelInvoke("hide");
 	die();
 }
