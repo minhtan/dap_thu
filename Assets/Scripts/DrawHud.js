8cd9dd35806fc03b7880033ecef773c4529edea6
tuongpm <phamminhtuong0123@gmail.com>
2014-11-13 09:14:44 +0700

    dang lam GUI

--- Assets/Scripts/DrawHud.js
+++ Assets/Scripts/DrawHud.js
@@ -2,19 +2,13 @@
 var originalWidth = 800;
 var originalHeight = 480;
 var mySkin : GUISkin;
-var ratio : int = 20;
+var ratioFontSize : int = 20;
 var fontSize : float = 20;
-private var widthScore;
-private var heightScore;
-var ratioScore :float = 4;
+private var widthScore : float;
+private var heightScore : float;
+var ratioScoreSize :float = 4;
+
 function Update(){
-	if(CheckScreenSize()){
-		widthScore = Screen.width / ratioScore;
-		heightScore =  Screen.height / ratioScore;
-	}else{
-		heightScore = originalHeight / ratioScore;
-		widthScore = originalWidth / ratioScore;
-	}
 }
 
 private function CheckScreenSize(): boolean{
@@ -27,34 +21,48 @@ private function CheckScreenSize(): boolean{
 private function AutoFontSize(): void
 {
 	if(CheckScreenSize()){
-		fontSize = Mathf.Min(Screen.width, Screen.height) / ratio;
+		fontSize = Mathf.Min(Screen.width, Screen.height) / ratioFontSize;
 	}
 }
 
-//private function AutoResizeGUI(): void{
-//	if(CheckScreenSize()){
-//		widthScore = Screen.width / ratioScore;
-//		heightScore =  Screen.height / ratioScore;
-//	}else{
-//		heightScore = originalHeight / ratioScore;
-//		widthScore = originalWidth / ratioScore;
-//	}
-//}
+private function AutoResizeGUI(): void{
+	if(CheckScreenSize()){
+		widthScore = Screen.width / ratioScoreSize;
+		heightScore =  Screen.height / ratioScoreSize;
+	}else{
+		heightScore = originalHeight / ratioScoreSize;
+		widthScore = originalWidth / ratioScoreSize;
+	}
+}
 
 function OnGUI(){
 	GUI.skin = mySkin;
 	GUI.contentColor = Color.red;
 	GUI.skin.label.fontSize = fontSize;
 	AutoFontSize();
-//	AutoResizeGUI();
+	AutoResizeGUI();
 	ShowScore(1000);
- 	
+	ShowGroupEvent();
+	BtnPause(calWidthGEvent());
 }
 
-
-
 private function ShowScore(score : int): void{
-	GUI.Label(Rect(0,0,widthScore,heightScore),"Score:" + score);
+	GUI.Label(Rect(0, 0, widthScore, heightScore),"Score:" + score);
+}
+
+private function BtnPause(width: float): void{
+	var posittionX : float = widthScore + width;
+	GUI.Button(Rect(posittionX, 0, widthScore, heightScore), "Pause");
+}
+
+private function calWidthGEvent() : float{
+	return Screen.width - (widthScore * 2);
+}
+
+private function ShowGroupEvent(): void{
+	var widthGEvent : float = calWidthGEvent();
+	GUI.BeginGroup(Rect(widthScore, 0, widthGEvent, heightScore));
+	GUI.EndGroup();
 }
 
 
