fa4d8be3ecef076924044be3bf581b581115cf72
tuongpm <phamminhtuong0123@gmail.com>
2014-11-14 17:35:49 +0700

    lam lai anchor gui game

--- Assets/Scripts/GUI Game/AnchorDelta.js
+++ Assets/Scripts/GUI Game/AnchorDelta.js
@@ -2,35 +2,25 @@
 
 var newAnchorX : float = 0;
 var anchorXDelta : float = 0;
-//var newAnchorXMax : float = 0.2;
+var anchorYDelta : float = 0;
 var newAnchorY : float = 0;
 function Start () {
- 	if(anchorXDelta != 0){
- 		setAnchorX(getRectTransform());
- 	}else{
- 		setAnchorY(getRectTransform());
+	setAnchor();
+}
+
+function setAnchor(){ 
+	var rectTrans : RectTransform = GetComponent.<RectTransform>();
+ 	if(anchorXDelta == 0){
+ 		newAnchorX = rectTrans.anchorMin.x;
+ 		anchorXDelta = rectTrans.anchorMax.x - newAnchorX;
  	}
-		
+ 	if(anchorYDelta == 0){
+		newAnchorY = rectTrans.anchorMin.y;
+ 		anchorYDelta = rectTrans.anchorMax.y - newAnchorY;
+ 	}
+	rectTrans.anchorMin = Vector2(newAnchorX, newAnchorY);
+	rectTrans.anchorMax = Vector2(newAnchorX + anchorXDelta,newAnchorY + anchorYDelta);
 }
 
-function getRectTransform(){
-	return GetComponent.<RectTransform>();	
-}
-
-function setAnchorX(rectTrans : RectTransform){ 
-	var anchorX : float;
-	rectTrans.anchorMin = Vector2(newAnchorX, 0);
-	rectTrans.anchorMax = Vector2(newAnchorX + anchorXDelta,1);
-}
-
-function setAnchorY(rectTrans : RectTransform){
-	var anchorY : float;
-	if(newAnchorY != 0){
-		anchorY = 1 - newAnchorY;
-	}else{
-		anchorY = rectTrans.anchorMin.y;
-	}
-	rectTrans.anchorMin = Vector2(0, anchorY);
-}
 
 
