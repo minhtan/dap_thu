8dbc06ab4a3b5576ea00becea72e01f420180b46
tuongpm <phamminhtuong0123@gmail.com>
2014-11-20 16:20:48 +0700

    show cac event

---
+++ Assets/Scripts/GUI Game/ShowEventX2.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var objControl : GameObject;
+private var gameControl : GameControl;
+var panel : GameObject;
+
+function Awake(){
+	gameControl = objControl.GetComponent.<GameControl>();
+}
+
+function Update() {
+	showVsHideEvent();
+}
+
+function showVsHideEvent(){
+	if(gameControl.isX2()){
+		panel.SetActive(true);
+	}else{
+		panel.SetActive(false);
+	}
+}
