96eb722f23bcba1ce6a5f40f24863c505f333fde
tuongpm <phamminhtuong0123@gmail.com>
2014-11-21 12:09:15 +0700

    sua 2 nut boost

--- Assets/Scripts/ControlButton/BtnSlowBoost.js
+++ Assets/Scripts/ControlButton/BtnSlowBoost.js
@@ -2,6 +2,8 @@
 
 var controlObject : GameObject;
 private var gameControl : GameControl;
+var objSlow : GameObject;
+var boostImg : Sprite; 
 
 function Awake(){
 	gameControl = controlObject.GetComponent.<GameControl>();
@@ -11,4 +13,5 @@ function boostSlow(){
 	PlayerControl.control.takeCoin();
 	PlayerControl.control.saveData();
 	gameControl.setSlowBoost();
+	objSlow.GetComponent.<UI.Image>().sprite = boostImg;
 }
--- Assets/Scripts/ControlButton/BtnX2Boost.js
+++ Assets/Scripts/ControlButton/BtnX2Boost.js
@@ -2,7 +2,8 @@
 
 var controlObject : GameObject;
 private var gameControl : GameControl;
-
+var objX2 : GameObject;
+var boostImg : Sprite; 
 function Awake(){
 	gameControl = controlObject.GetComponent.<GameControl>();
 }
@@ -11,4 +12,5 @@ function boostX2(){
 	PlayerControl.control.takeCoin();
 	PlayerControl.control.saveData();
 	gameControl.setX2Boost();
+	objX2.GetComponent.<UI.Image>().sprite = boostImg;
 }
