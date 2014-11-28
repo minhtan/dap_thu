5d69b9948c3775a59e77fc71ca37f2e47df483fc
tuongpm <phamminhtuong0123@gmail.com>
2014-11-28 15:28:46 +0700

    sua nut startGame khong hien panel End coin

--- Assets/Scripts/ControlButton/BtnStartGame.js
+++ Assets/Scripts/ControlButton/BtnStartGame.js
@@ -3,17 +3,12 @@
 var controlObject : GameObject;
 private var gameControl : GameControl;
 var panelHide : GameObject;
-var panelShow : GameObject;
 
 function Awake(){
 	gameControl = controlObject.GetComponent.<GameControl>();
 }
 
 function startClick(){
-	if(PlayerControl.control.takeCoin()){
 		gameControl.startGame();
 		panelHide.SetActive(false);
-	}else{
-		panelShow.SetActive(true);
-	}
 }
